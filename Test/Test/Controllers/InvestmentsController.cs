d98aa6a7ce2b855605724dec066cc860fcd3bb1d
Hemal <hemal@telkomsa.net>
2018-08-16 20:50:27 +0200

    Connected db and scaffolded Investments.

---
+++ Test/Test/Controllers/InvestmentsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    public class InvestmentsController : Controller
+    {
+        private StokvelEntities db = new StokvelEntities();
+
+        // GET: Investments
+        public ActionResult Index()
+        {
+            return View(db.Investments.ToList());
+        }
+
+        // GET: Investments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Investment investment = db.Investments.Find(id);
+            if (investment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(investment);
+        }
+
+        // GET: Investments/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Investments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Investment_ID,Date_Opened,Date_Ending,Interest,Monthly_Contribution,Period,Expected_Amount")] Investment investment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Investments.Add(investment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(investment);
+        }
+
+        // GET: Investments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Investment investment = db.Investments.Find(id);
+            if (investment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(investment);
+        }
+
+        // POST: Investments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Investment_ID,Date_Opened,Date_Ending,Interest,Monthly_Contribution,Period,Expected_Amount")] Investment investment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(investment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(investment);
+        }
+
+        // GET: Investments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Investment investment = db.Investments.Find(id);
+            if (investment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(investment);
+        }
+
+        // POST: Investments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Investment investment = db.Investments.Find(id);
+            db.Investments.Remove(investment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
