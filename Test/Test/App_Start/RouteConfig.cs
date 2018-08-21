476e5eb974c36c656dbb81740cef2abb958307c2
masudh <masudh@LENOVO-PC>
2018-08-21 13:19:33 +0200

    Your app  is faulty

--- Test/Test/App_Start/RouteConfig.cs
+++ Test/Test/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Test
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Investments", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
